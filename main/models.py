fca06902601f1866522d8246aa78e4e7773186be
RizvanuSN <rizvanusn@gmail.com>
2023-12-19 17:53:24 +0700

    tes

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255) 
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
