380efbe0075f29693754f07657c225c011b86060
galentaris <galentaris01@gmail.com>
2023-12-20 15:52:33 +0700

    tes

--- main/models.py
+++ main/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)  
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    price = models.IntegerField() 
     description = models.TextField()
